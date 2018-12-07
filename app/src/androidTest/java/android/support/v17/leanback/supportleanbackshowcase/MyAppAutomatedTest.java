265c17626614e456e0076e25261491276ddf3a77
mankidal19 <man.kidal19@gmail.com>
2018-12-07 16:41:49 +0800

    add method to find desired content on rows

--- app/src/androidTest/java/android/support/v17/leanback/supportleanbackshowcase/MyAppAutomatedTest.java
+++ app/src/androidTest/java/android/support/v17/leanback/supportleanbackshowcase/MyAppAutomatedTest.java
@@ -18,6 +18,7 @@ import org.junit.runner.RunWith;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.BySelector;
 import androidx.test.uiautomator.UiCollection;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
@@ -118,7 +119,10 @@ public class MyAppAutomatedTest {
         //navigate to TV SHOWS
         navigateMenu(TV_SHOW);
 
+        //wait until TV SHOW fragment loaded
+        mDevice.wait(Until.hasObject(By.desc(TV_SHOW_FRAGMENT)),LAUNCH_TIMEOUT);
 
+        findInGrid(TV_SHOW_CONTENT);
 
     }
 
@@ -134,7 +138,7 @@ public class MyAppAutomatedTest {
             UiObject headerName = list.getChild( new UiSelector().index(i))
                     .getChild(new UiSelector().className(android.widget.TextView.class));
             name = headerName.getText();
-            if(!name.equals(TV_SHOW)){
+            if(!name.equals(menuName)){
                 mDevice.pressDPadDown();
             }
             else {
@@ -144,6 +148,80 @@ public class MyAppAutomatedTest {
         }
     }
 
+
+    private void findInGrid(String showName) throws UiObjectNotFoundException {
+
+        UiSelector uiSelector = new UiSelector().className(android.widget.FrameLayout.class)
+                .childSelector(new UiSelector().className(android.widget.TextView.class)
+                        .text(showName));
+
+        boolean isMovingRight = true;
+        UiObject previous = null;
+        UiObject current;
+
+        boolean isEndReached = false;
+
+        String targetShow = null;
+        String previousTitle = null;
+        String currentTitle = null;
+
+        do{
+
+            current = mDevice.findObject(new UiSelector().className(android.widget.FrameLayout.class).focused(true).
+                    childSelector(new UiSelector().className(android.widget.TextView.class)));
+            currentTitle = current.getText();
+            if(mDevice.findObject(uiSelector).exists()){
+                targetShow = mDevice.findObject(uiSelector).getText();
+            }
+
+            System.out.println("current title: " + current.getText());
+
+            if(previous!=null){
+                System.out.println("previous title: " + previousTitle);
+
+                if(currentTitle.equals(previousTitle)){
+                    mDevice.pressDPadDown();
+                    mDevice.waitForWindowUpdate(null,500);
+
+                    current = mDevice.findObject(new UiSelector().className(android.widget.FrameLayout.class).focused(true).
+                            childSelector(new UiSelector().className(android.widget.TextView.class)));
+                    currentTitle = current.getText();
+
+                    if(currentTitle.equals(previousTitle)){
+                        if (isEndReached) {
+                            throw new RuntimeException("Show was not found!");
+                        } else {
+                            isEndReached = true;
+                        }
+                    }
+                }
+            }
+
+            previous = current;
+            previousTitle = previous.getText();
+
+            if(!currentTitle.equals(targetShow)){
+                moveFocus(isMovingRight);
+            }
+
+
+            System.out.println("current: " + current.getText() + ", target: " + targetShow);
+        }while(!currentTitle.equals(targetShow));
+
+        mDevice.pressDPadCenter();
+
+    }
+
+    private void moveFocus(boolean movingRight){
+        if(movingRight){
+            mDevice.pressDPadRight();
+        }
+        else
+        {
+            mDevice.pressDPadLeft();
+        }
+        mDevice.waitForWindowUpdate(null,500);
+    }
 
     private String getLauncherPackageName() {
         // Create launcher Intent
