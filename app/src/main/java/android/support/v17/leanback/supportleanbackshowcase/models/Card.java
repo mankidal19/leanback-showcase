38729b33b17417b3d0e1d06a6dabf399927fb368
mankidal19 <man.kidal19@gmail.com>
2018-12-26 16:16:33 +0800

    add getContentProvider method

--- app/src/main/java/android/support/v17/leanback/supportleanbackshowcase/models/Card.java
+++ app/src/main/java/android/support/v17/leanback/supportleanbackshowcase/models/Card.java
@@ -193,6 +193,10 @@ public class Card implements Serializable {
         return mLive;
     }
 
+    public Provider getContentProvider(){
+        return mContentProvider;
+    }
+
     public enum Type {
 
         MOVIE_COMPLETE,
