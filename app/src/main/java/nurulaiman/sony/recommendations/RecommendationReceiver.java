5a8a45d08c3d06c214a03a0b302300ab017571d3
mankidal19 <man.kidal19@gmail.com>
2019-01-03 16:28:27 +0800

    add string TAG

--- app/src/main/java/nurulaiman/sony/recommendations/RecommendationReceiver.java
+++ app/src/main/java/nurulaiman/sony/recommendations/RecommendationReceiver.java
@@ -5,18 +5,23 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /*
  * This class extends BroadcastReceiver and publishes Recommendations when received.
  */
 public class RecommendationReceiver extends BroadcastReceiver {
     private static final long INITIAL_DELAY = 5000;
+    private static final String TAG = "RecommendationReceiver";
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             scheduleRecommendationUpdate(context);
+            Log.d(TAG,"Receive intent boot completed");
         }
+        Log.d(TAG,"Receive intent");
     }
 
     private void scheduleRecommendationUpdate(Context context) {
