b146278b661bc4f9df25983887fcf57c2200b930
mankidal19 <man.kidal19@gmail.com>
2019-01-03 09:21:54 +0800

    create Utils class to setup UI
    **removing repetitive code

---
+++ app/src/main/java/nurulaiman/sony/utils/AppUiUtils.java
@@ -0,0 +1,53 @@
+package nurulaiman.sony.utils;
+
+import android.content.Context;
+import android.support.v17.leanback.supportleanbackshowcase.R;
+import android.support.v17.leanback.widget.RowPresenter;
+import android.view.View;
+
+import nurulaiman.sony.fragment.MySettingsFragment;
+
+
+public class AppUiUtils {
+
+    private Context context = null;
+    private String provider;
+    private String interfaceMode;
+
+    public AppUiUtils(Context context){
+        this.context = context;
+        provider = MySettingsFragment.getDefaults("pref_providers_key",context);
+        interfaceMode = MySettingsFragment.getDefaults("pref_interface_key",context);
+    }
+
+    public void setDetailsPageBg(View actionsView, RowPresenter.ViewHolder viewHolder){
+        if(provider.equals("fptplay")){
+            actionsView.setBackgroundColor(context.getResources().
+                    getColor(R.color.fptplay_detail_view_actionbar_background));
+
+            View detailsView = viewHolder.view.findViewById(R.id.details_frame);
+            detailsView.setBackgroundColor(
+                    context.getResources().getColor(R.color.fptplay_detail_view_background));
+        }
+
+        else if(provider.equals("hotstar")){
+            actionsView.setBackgroundColor(context.getResources().
+                    getColor(R.color.hotstar_detail_view_actionbar_background));
+
+            View detailsView = viewHolder.view.findViewById(R.id.details_frame);
+            detailsView.setBackgroundColor(
+                    context.getColor(R.color.hotstar_detail_view_background));
+        }
+
+        else{
+            actionsView.setBackgroundColor(context.getResources().
+                    getColor(R.color.detail_view_actionbar_background));
+
+            View detailsView = viewHolder.view.findViewById(R.id.details_frame);
+            detailsView.setBackgroundColor(
+                    context.getColor(R.color.detail_view_background));
+        }
+    }
+
+
+}
