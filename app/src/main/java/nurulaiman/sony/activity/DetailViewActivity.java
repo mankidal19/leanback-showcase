51fef6c406a673ab9c060be4f53cd2186c659014
mankidal19 <man.kidal19@gmail.com>
2019-01-04 09:19:12 +0800

    reconstruct code by combining 3 detailView activity for each content type into 1 activity only

---
+++ app/src/main/java/nurulaiman/sony/activity/DetailViewActivity.java
@@ -0,0 +1,87 @@
+package nurulaiman.sony.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v17.leanback.supportleanbackshowcase.R;
+import android.support.v17.leanback.supportleanbackshowcase.models.Card;
+import android.support.v17.leanback.supportleanbackshowcase.models.DetailedCard;
+import android.util.Log;
+import android.view.KeyEvent;
+
+import nurulaiman.sony.fragment.DetailViewLiveBroadcastFragment;
+import nurulaiman.sony.fragment.DetailViewMovieFragment;
+import nurulaiman.sony.fragment.DetailViewTvShowFragment;
+import nurulaiman.sony.fragment.MyDetailsFragment;
+import nurulaiman.sony.utils.MatchingCardUtils;
+
+
+//reconstruct code by combining 3 detailView activity for each content type into 1 activity only
+public class DetailViewActivity extends LeanbackActivity {
+    //for finding matching detailed card
+    private MatchingCardUtils matchingCardUtils = null;
+    private DetailedCard chosenCard = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        matchingCardUtils = new MatchingCardUtils(this);
+        chosenCard = matchingCardUtils.findMatchingCard(getIntent().getExtras().getString("videoTitle"));
+
+        setContentView(R.layout.activity_detail_example);
+
+
+        if (savedInstanceState == null) {
+
+
+            MyDetailsFragment fragment;
+
+            if(chosenCard.getType().equals(Card.Type.LIVE_TV)){
+                fragment = new DetailViewLiveBroadcastFragment();
+            }
+
+            else if(chosenCard.getType().equals(Card.Type.MOVIE)){
+                fragment = new DetailViewMovieFragment();
+            }
+
+            else{
+                fragment = new DetailViewTvShowFragment();
+            }
+
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.details_fragment, fragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int KeyCode, KeyEvent event){
+
+        boolean handled = false;
+        //KeyCode = event.getKeyCode();
+        Intent intent = new Intent(this, LiveActivity.class);
+
+        Log.i("KeyEvent",KeyCode + " button pressed");
+
+
+        if(KeyCode == KeyEvent.KEYCODE_ESCAPE){
+            Log.i("KeyEvent","Exit button pressed");
+            handled=true;
+            moveTaskToBack(true);
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+
+        }
+
+
+        if(handled){
+
+            return handled;
+        }
+
+        else{
+            return super.onKeyDown(KeyCode, event);
+        }
+        //return handled;
+    }
+}
